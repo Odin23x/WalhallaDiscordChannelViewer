f8fe04ffee4333bca483dc914c3c083f53b41417
Odin23x <99129627+Odin23x@users.noreply.github.com>
2026-06-10 09:40:16 +0200

    Remove start.bat

--- start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo [%DATE% %TIME%] BAT gestartet >> "%~dp0startup.log"
-powershell.exe -NonInteractive -ExecutionPolicy Bypass -File "%~dp0DiscordChannelViewer.ps1"
-echo [%DATE% %TIME%] PS beendet: %ERRORLEVEL% >> "%~dp0startup.log"
